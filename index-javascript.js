2751677ecec49ce6a96245be0565636c273357ad
Bayo17 <bayoumyhazem@itis-molinari.eu>
2024-11-04 15:43:27 +0100

    test

--- index-javascript.js
+++ index-javascript.js
@@ -1,32 +1,4 @@
-const createTable = () =>{
-    idTabella.innerHTML = tabella
-}
-
-const idTabella = document.getElementById("tabella")
-
-const tabella = `<table class="tabella">
-                <td>
-                </td>
-                <th>
-                    Lunedì 
-                </th>
-                <th>
-                    Martedì
-                </th>
-                <th>
-                    Mercoledì
-                </th>
-                <th>
-                    Giovedì
-                </th>
-                <th>
-                    Venerdì
-                </th>
-                <tr><td class="miao">8</td></tr>
-                <tr><td class="miao">9</td></tr>
-                <tr><td class="miao">10</td></tr>
-                <tr><td class="miao">11</td></tr>
-                <tr><td class="miao">12</td></tr>
-              </table>`
-
-createTable()
+const tipologieVisite = ["Cardiologia", "Psicologia", "Oncologia", "Ortopedia", "Neurologia"];
+let prenotazioni = {};
+let tipologiaCorrente = tipologieVisite[0];  // Tipologia di visita selezionata di default
+let dataCorrente = moment().startOf('isoWeek'); // Data di inizio della settimana corrente (lunedì)
