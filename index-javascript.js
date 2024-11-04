cbd3ab7f4c1390e3396d8709ef8ffc1e57b372dd
Bayo17 <bayoumyhazem@itis-molinari.eu>
2024-11-04 15:50:53 +0100

    test

--- index-javascript.js
+++ index-javascript.js
@@ -1,4 +1,4 @@
 const tipologieVisite = ["Cardiologia", "Psicologia", "Oncologia", "Ortopedia", "Neurologia"];
 let prenotazioni = {};
-let tipologiaCorrente = tipologieVisite[0];  // Tipologia di visita selezionata di default
-let dataCorrente = moment().startOf('isoWeek'); // Data di inizio della settimana corrente (lunedì)
+let tipologiaCorrente = tipologieVisite[0];  
+let dataCorrente = moment().startOf('isoWeek');
